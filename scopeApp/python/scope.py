bcbab70abb853d8bf41f682dee7b8d47e1a3576c
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2016-03-01 10:38:02 +0100

    retrieve vairable name and data type by parsing epics template

--- scopeApp/python/scope.py
+++ scopeApp/python/scope.py
@@ -1,6 +1,106 @@
 
 import param # param is an extension in asynPythonDriver library.
 
+import pyparsing as pp
+def parseDb(content):
+    """
+    Parse an EPICS.db file.
+
+    Return a list of EPICS record dictionaries. Each record dict contains the EPICS-field
+    and value pairs and additional keys for:
+
+    - recordname
+    - alias
+    - RTYP
+    - info {INFOKEY:INFOVALUE,}
+    """
+    recordName= pp.ZeroOrMore(pp.Suppress(pp.Word('"'))) +\
+                pp.Word(pp.alphanums+"_-+:[]<>;$()") +\
+                pp.ZeroOrMore(pp.Suppress(pp.Word('"')))
+    qString   = pp.dblQuotedString.setParseAction(pp.removeQuotes) # double quoted String, quotes removed
+
+    comment   = pp.Suppress(pp.pythonStyleComment)
+    alias     = pp.Group(pp.Keyword("alias") +\
+                pp.Suppress(pp.Word("(")) +\
+                recordName +  pp.Suppress(pp.Word(",")) +\
+                recordName + pp.Suppress(pp.Word(")")) )
+    field     = pp.Group((pp.Keyword("field")^pp.Keyword("info")) +\
+                pp.Suppress(pp.Word("(")) + pp.Word(pp.alphanums) + pp.Suppress(pp.Word(",")) + qString + pp.Suppress(pp.Word(")")) +\
+                pp.ZeroOrMore(comment) ^\
+                pp.Keyword("alias") + pp.Suppress(pp.Word("(")) + recordName +  pp.Suppress(pp.Word(")")) + pp.ZeroOrMore(comment))
+    record    = pp.Group(pp.Keyword("record") + pp.Suppress(pp.Word("(")) +\
+                    pp.Word(pp.alphanums) + pp.Suppress(pp.Word(",")) + recordName +\
+                pp.Suppress(pp.Word(")")) + pp.ZeroOrMore(comment) +\
+                pp.Suppress(pp.Word("{")) + pp.ZeroOrMore(comment) + pp.Group(pp.ZeroOrMore(field)) + pp.Suppress(pp.Word("}")))
+    epicsDb   = pp.OneOrMore(comment ^ alias ^ record)
+
+    recordList = []
+    for pGroup in epicsDb.parseString(content):
+        if pGroup[0] == 'record':
+            rec = {'RTYP':pGroup[1],'recordname':pGroup[2]}
+            if len(pGroup) <= 3:
+                    continue
+            for fields in pGroup[3]:
+                if fields[0] == 'field':     # ['field','fieldType','fieldValue']
+                    rec[fields[1]]=fields[2]
+                if fields[0] == 'alias':
+                    rec['alias'] = fields[1]
+                if fields[0] == 'info':
+                    if not rec.has_key('info'):
+                        rec['info'] = {}
+                    rec['info'][fields[1]] = fields[2]
+            recordList.append(rec)
+    return recordList
+
+import re
+def findParams(records):
+    # regexp patterns to remove macros like $(P)
+    macro_pat = re.compile('\$\(.*?\)')
+    # regexp pattern to extract drv info string 
+    link_pat = re.compile('^@asyn\(.*\)\s*(.*)')
+    # extract data type info from device type (DTYP)
+    dtype_pat = re.compile('^asyn(.*?)(|In|Out|Read|Write)$')
+
+    params = {}
+    for record in records:
+        recname = macro_pat.sub('', record['recordname'])
+        # strip off _RBV suffix, this is only a convention
+        recname = recname.rstrip('_RBV')
+
+        # check dtype is asynXXX(In|Out|Read|Write)
+        m = dtype_pat.match(record.get('DTYP', ''))
+        if m is None or m.group(1) == '':
+            continue
+        dtype = m.group(1)
+
+        # get INP or OUT link
+        if 'INP' in record:
+            iolink = record['INP']
+        elif 'OUT' in record:
+            iolink = record['OUT']
+        else:
+            continue
+        # check link is of form @asyn()drvinfo
+        m = link_pat.match(iolink)
+        if m is None or m.group(1) == '':
+            continue
+        drvinfo = m.group(1)
+
+        # try 'pyname' info
+        pyname = ''
+        if 'info' in record:
+            if 'pyname' in record['info']:
+                pyname = record['info']['pyname']
+
+        if drvinfo not in params:
+            if pyname != '':
+                name = pyname
+            else:
+                name = recname
+            params[drvinfo] = (name, dtype)
+    return params
+
+
 class Param(object):
     """
     A descriptor class to ease parameter access.
@@ -40,15 +140,35 @@ FREQUENCY=1000 #      /* Frequency in Hz */
 AMPLITUDE=1.0  #      /* Plus and minus peaks of sin wave */
 NUM_DIVISIONS=10 #    /* Number of scope divisions in X and Y */
 
+DataType = {
+    'Int32'        : param.Int32,
+    'Float64'      : param.Float64,
+    'Octet'        : param.Octet,
+    'Int8Array'    : param.Int8Array,
+    'Int16Array'   : param.Int16Array,
+    'Int32Array'   : param.Int32Array,
+    'Float32Array' : param.Float32Array,
+    'Float64Array' : param.Float64Array,
+}
+
 
 class PythonDriverMeta(type):
     def __new__(self, clsname, base, clsdict):
         # Create the class definition
         cls = type.__new__(self, clsname, base, clsdict)
+        # if database file is defined, load params from it
+        if hasattr(cls, '__db__'):
+            dbfile = getattr(cls, '__db__')
+            records = parseDb(open(dbfile).read())
+            params = findParams(records)
+            for drvinfo, info in params.items():
+                param =  Param(drvinfo, DataType[info[1]])
+                setattr(cls, info[0], param)
+
         # Find Param instances and assocate the variable name with its Param name
         # It serves as a lookup table
         params = {}
-        for name, value in clsdict.items():
+        for name, value in vars(cls).items():
             if isinstance(value, Param):
                 params[value.name] = name
         setattr(cls, 'params', params)
@@ -71,27 +191,7 @@ class ScopeDriver(PythonDriver):
     # The first argument is the drvInfo string and the second is the data type
     # Ideally this could be parsed from EPICS database.
     # The variable name could be inferred from record name or the info fields, e.g. info(pyname, "x")
-    run = Param("SCOPE_RUN", param.Int32)
-    npoints = Param("SCOPE_MAX_POINTS", param.Int32)
-    refresh = Param("SCOPE_UPDATE_TIME", param.Float64)
-
-    mini = Param("SCOPE_MIN_VALUE",  param.Float64)
-    maxi = Param("SCOPE_MAX_VALUE",  param.Float64)
-    mean = Param("SCOPE_MEAN_VALUE",  param.Float64)
-
-    waveform = Param("SCOPE_WAVEFORM",  param.Float64Array)
-    timebase = Param("SCOPE_TIME_BASE",  param.Float64Array)
-
-    noise = Param("SCOPE_NOISE_AMPLITUDE", param.Float64)
-    delay = Param("SCOPE_TRIGGER_DELAY", param.Float64)
-    
-    offset = Param("SCOPE_VOLT_OFFSET", param.Float64)
-    gain_menu = Param("SCOPE_VERT_GAIN_SELECT", param.Int32)
-    gain = Param("SCOPE_VERT_GAIN", param.Float64)
-    voltsdiv_menu = Param("SCOPE_VOLTS_PER_DIV_SELECT", param.Int32)
-    voltsdiv = Param("SCOPE_VOLTS_PER_DIV", param.Float64)
-    timediv_menu = Param("SCOPE_TIME_PER_DIV_SELECT", param.Int32)
-    timediv = Param("SCOPE_TIME_PER_DIV", param.Float64)
+    __db__ = '/Users/wang/Development/asynPythonDriver/db/scope.template'
 
     def __init__(self):
         # Initialize parameters
