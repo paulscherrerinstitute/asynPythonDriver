a6dc86cfd9c3aefadd7ebaad1f20c515b55d45ef
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2016-06-19 21:41:38 +0200

    add a simplistic example

--- iocs/scopeIOC/iocBoot/iocscope/st.cmd
+++ iocs/scopeIOC/iocBoot/iocscope/st.cmd
@@ -14,10 +14,12 @@ cd ${TOP}
 dbLoadDatabase "dbd/scope.dbd"
 scope_registerRecordDeviceDriver pdbbase
 
-asynPythonDriverConfigure("PY1", "scope", 17)
+asynPythonDriverConfigure("PY1", "scope", 18)
+asynPythonDriverConfigure("PY2", "test", 1)
 
 ## Load record instances
 dbLoadRecords("${TOP}/db/scope.template", "P=MTEST:,R=SCOPE:,PORT=PY1,ADDR=0,NPOINTS=1000,TIMEOUT=1")
+dbLoadRecords("${TOP}/db/test.template", "PORT=PY2,ADDR=0,TIMEOUT=1")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
